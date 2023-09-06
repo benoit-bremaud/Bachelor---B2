902c780e66f53637f97371bb3c1ee4585ad65323
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-09-06 11:40:19 +0200

    feat(job06): Add support for sorting strings alphabetically in my_array_sort_bonus()
    In this commit, we have extended the functionality of the 'my_array_sort_bonus()' function to include the ability to sort strings alphabetically in both ascending ('ASC') and descending ('DESC') orders. This enhancement enhances the versatility of the function, allowing it to sort arrays containing various data types, including numbers and strings.
    
    Examples demonstrating the sorting of arrays with numeric and string values in ascending and descending orders have been provided. The code includes explanations and comments to ensure clarity.
    
    This addition provides users with the flexibility to sort arrays with mixed data types, making 'my_array_sort_bonus()' a more versatile sorting function.

--- runtrack-b2-php/Jour01/Job06/index.php
+++ runtrack-b2-php/Jour01/Job06/index.php
@@ -70,4 +70,61 @@ echo "<h3>implode()</h3>";
 echo "<p><strong>implode()</strong> est une fonction PHP qui est utilisée pour concaténer les éléments d'un tableau en une seule chaîne de caractères, en les séparant par un délimiteur spécifié.
 Elle prend deux arguments : le premier est le délimiteur (une chaîne de caractères) qui sera inséré entre chaque élément du tableau dans la chaîne résultante, et le deuxième est le tableau contenant les éléments à concaténer.</p>";
 echo "<p><strong>implode()</strong> est couramment utilisée pour afficher des listes d'éléments à partir d'un tableau sous une forme lisible par l'homme. Par exemple, elle peut être utilisée pour afficher une liste de noms séparés par des virgules.</p>";
+
+echo "<h1>Bonus</h1>";
+// Fonction my_array_sort() pour trier un tableau dans l'ordre croissant ou décroissant, y compris les chaînes de caractères.
+function my_array_sort_bonus(array $arrayToSort, string $order): array {
+    $size = count($arrayToSort);
+
+    // Vérifie si le paramètre d'ordre est "ASC" (croissant) ou "DESC" (décroissant).
+    if ($order === "ASC") {
+        // Tri dans l'ordre croissant.
+        sort($arrayToSort);
+    } elseif ($order === "DESC") {
+        // Tri dans l'ordre décroissant.
+        rsort($arrayToSort);
+    } else {
+        // Cas où l'ordre n'est ni "ASC" ni "DESC", retourne le tableau non trié.
+        return $arrayToSort;
+    }
+
+    return $arrayToSort; // Renvoie le tableau trié.
+}
+
+// Exemple d'utilisation de la fonction pour trier un tableau dans l'ordre croissant (numérique et alphabétique).
+$arrayToSort = [5, 2, "apple", 9, 1, "banana", 5, "cherry", 6];
+$sortedArrayAsc = my_array_sort_bonus($arrayToSort, "ASC");
+echo "<h2>Tri dans l'ordre croissant :</h2>";
+
+// Affichage avec print_r().
+echo "<h3>Avec print_r() :</h3>";
+print_r($sortedArrayAsc);
+
+// Affichage avec implode().
+echo "<h3>Avec implode() :</h3>";
+echo implode(', ', $sortedArrayAsc);
+
+// Exemple d'utilisation de la fonction pour trier un tableau dans l'ordre décroissant (numérique et alphabétique).
+$sortedArrayDesc = my_array_sort_bonus($arrayToSort, "DESC");
+echo "<h2>Tri dans l'ordre décroissant :</h2>";
+
+// Affichage avec print_r().
+echo "<h3>Avec print_r() :</h3>";
+print_r($sortedArrayDesc);
+
+// Affichage avec implode().
+echo "<h3>Avec implode() :</h3>";
+echo implode(', ', $sortedArrayDesc);
+
+// Définitions des fonctions 'print_r' et 'implode'.
+echo "<h2>Différences entre 'print_r()' et 'implode()'</h2>";
+echo "<h3>print_r()</h3>";
+echo "<p><strong>print_r()</strong> est une fonction PHP qui est principalement utilisée pour afficher des informations lisibles par l'homme sur une variable, généralement un tableau. Elle prend en argument la variable que vous souhaitez afficher et la formate de manière lisible, avec une indentation pour les tableaux multidimensionnels, ce qui facilite la compréhension de la structure de la variable.</p>";
+echo "<p><strong>print_r()</strong> est utile pour le débogage et l'inspection de variables complexes, car elle affiche les clés et les valeurs associées pour les tableaux, les objets et les structures de données.</p>";
+echo "<h3>implode()</h3>";
+echo "<p><strong>implode()</strong> est une fonction PHP qui est utilisée pour concaténer les éléments d'un tableau en une seule chaîne de caractères, en les séparant par un délimiteur spécifié.
+Elle prend deux arguments : le premier est le délimiteur (une chaîne de caractères) qui sera inséré entre chaque élément du tableau dans la chaîne résultante, et le deuxième est le tableau contenant les éléments à concaténer.</p>";
+echo "<p><strong>implode()</strong> est couramment utilisée pour afficher des listes d'éléments à partir d'un tableau sous une forme lisible par l'homme. Par exemple, elle peut être utilisée pour afficher une liste de noms séparés par des virgules.</p>";
+
+
 ?>
