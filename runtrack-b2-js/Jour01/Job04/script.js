e8863abdf426f0b72ce2a7b5f7b11d770f45bd61
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-09-07 11:49:34 +0200

    refactor: Modify code to display array elements and sum
    - Updated the existing code to display individual elements of the array on the web page before calculating and displaying the sum.

--- runtrack-b2-js/Jour01/Job04/script.js
+++ runtrack-b2-js/Jour01/Job04/script.js
@@ -8,14 +8,27 @@ function myArraySum(array) {
     // Initialisation d'une variable pour stocker la somme.
     let sum = 0;
 
+    // Création d'une chaîne de caractères pour stocker les éléments du tableau.
+    let elementsString = "";
+
     // Boucle pour parcourir les éléments du tableau.
     for (let i = 0; i < array.length; i++) {
+        // Ajout de l'élément actuel à la chaîne de caractères.
+        elementsString += array[i];
+        // Ajout d'une virgule et d'un espace sauf pour le dernier élément.
+        if (i < array.length - 1) {
+            elementsString += ", ";
+        }
+        
         // Addition de l'élément actuel à la somme.
         sum += array[i];
     }
 
-    // Retourne la somme calculée.
-    return sum;
+    // Retourne un objet avec la somme et les éléments du tableau.
+    return {
+        sum: sum,
+        elementsString: elementsString
+    };
 }
 
 // Exemple d'utilisation de la fonction :
@@ -23,7 +36,8 @@ const numbers = [1, 2, 3, 4, 5];
 const result = myArraySum(numbers);
 
 // Affichage du résultat dans la console.
-console.log("La somme des éléments du tableau est : " + result);
+console.log("La somme des éléments du tableau est : " + result.sum);
 
 // Affichage du résultat sur la page web.
-document.getElementById("result").textContent = "La somme des éléments du tableau est : " + result;
+document.getElementById("elements").textContent = "Éléments du tableau : " + result.elementsString;
+document.getElementById("result").textContent = "La somme des éléments du tableau est : " + result.sum;
