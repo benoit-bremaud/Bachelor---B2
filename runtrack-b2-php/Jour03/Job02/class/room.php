357eb751fd55db3db614878d4a36bdaa0c8976d2
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-09-08 15:54:07 +0200

    refactor(Room): Enhance Room class with private properties and getters/setters
    This commit refactors the Room class by making its properties private and implementing getters/setters for each property. It improves code organization and adheres to best practices.

--- runtrack-b2-php/Jour03/Job02/class/room.php
+++ runtrack-b2-php/Jour03/Job02/class/room.php
@@ -1,59 +1,61 @@
 <?php
 
 class Room {
-    // Attributs privés pour représenter les données de la table 'room'
-    private $id;          // Identifiant unique de la salle (entier)
-    private $floor_id;    // Identifiant du niveau auquel la salle est située (entier)
-    private $name;        // Nom de la salle (chaîne de caractères)
-    private $capacity;    // Capacité de la salle (entier)
+    private $id;
+    private $floorId;
+    private $name;
+    private $capacity;
 
     // Constructeur avec des paramètres optionnels
-    public function __construct($id = null, $floor_id = null, $name = null, $capacity = null) {
-        // Initialisation des attributs avec les valeurs passées en paramètre
+    public function __construct(
+        int $id = null,
+        int $floorId = null,
+        string $name = null,
+        int $capacity = null
+    ) {
         $this->id = $id;
-        $this->floor_id = $floor_id;
+        $this->floorId = $floorId;
         $this->name = $name;
         $this->capacity = $capacity;
     }
 
-    // Fonction pour obtenir l'ID de la salle
-    public function getId() {
+    // Getter pour l'ID de la salle
+    public function getId(): ?int {
         return $this->id;
     }
 
-    // Fonction pour obtenir l'ID du niveau auquel la salle est située
-    public function getFloorId() {
-        return $this->floor_id;
+    // Getter pour l'ID de l'étage auquel la salle est située
+    public function getFloorId(): ?int {
+        return $this->floorId;
     }
 
-    // Fonction pour obtenir le nom de la salle
-    public function getName() {
+    // Setter pour l'ID de l'étage auquel la salle est située
+    public function setFloorId(int $floorId): self {
+        $this->floorId = $floorId;
+        return $this;
+    }
+
+    // Getter pour le nom de la salle
+    public function getName(): ?string {
         return $this->name;
     }
 
-    // Fonction pour obtenir la capacité de la salle
-    public function getCapacity() {
+    // Setter pour le nom de la salle
+    public function setName(string $name): self {
+        $this->name = $name;
+        return $this;
+    }
+
+    // Getter pour la capacité de la salle
+    public function getCapacity(): ?int {
         return $this->capacity;
     }
 
-    // Fonction pour définir l'ID de la salle
-    public function setId($id) {
-        $this->id = $id;
-    }
-
-    // Fonction pour définir l'ID du niveau auquel la salle est située
-    public function setFloorId($floor_id) {
-        $this->floor_id = $floor_id;
-    }
-
-    // Fonction pour définir le nom de la salle
-    public function setName($name) {
-        $this->name = $name;
-    }
-
-    // Fonction pour définir la capacité de la salle
-    public function setCapacity($capacity) {
+    // Setter pour la capacité de la salle
+    public function setCapacity(int $capacity): self {
         $this->capacity = $capacity;
+        return $this;
     }
 }
+
 ?>
