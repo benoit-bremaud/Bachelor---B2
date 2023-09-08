771d4c7ccdd9b6e864d14635688953262bffae34
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-09-08 15:53:34 +0200

    refactor(Floor): Improve Floor class with private properties and getters/setters
    This commit refactors the Floor class by making its properties private and implementing getters/setters for each property. It enhances code organization and adheres to best practices.

--- runtrack-b2-php/Jour03/Job02/class/floor.php
+++ runtrack-b2-php/Jour03/Job02/class/floor.php
@@ -1,47 +1,47 @@
 <?php
 
 class Floor {
-    // Attributs privés pour représenter les données de la table 'floor'
-    private $id;      // Identifiant unique de l'étage (entier)
-    private $name;    // Nom de l'étage (chaîne de caractères)
-    private $level;   // Niveau de l'étage (entier)
+    private $id;
+    private $name;
+    private $level;
 
     // Constructeur avec des paramètres optionnels
-    public function __construct($id = null, $name = null, $level = null) {
-        // Initialisation des attributs avec les valeurs passées en paramètre
+    public function __construct(
+        int $id = null,
+        string $name = null,
+        int $level = null
+    ) {
         $this->id = $id;
         $this->name = $name;
         $this->level = $level;
     }
 
-    // Fonction pour obtenir l'ID de l'étage
-    public function getId() {
+    // Getter pour l'ID de l'étage
+    public function getId(): ?int {
         return $this->id;
     }
 
-    // Fonction pour obtenir le nom de l'étage
-    public function getName() {
+    // Getter pour le nom de l'étage
+    public function getName(): ?string {
         return $this->name;
     }
 
-    // Fonction pour obtenir le niveau de l'étage
-    public function getLevel() {
+    // Setter pour le nom de l'étage
+    public function setName(string $name): self {
+        $this->name = $name;
+        return $this;
+    }
+
+    // Getter pour le niveau de l'étage
+    public function getLevel(): ?int {
         return $this->level;
     }
 
-    // Fonction pour définir l'ID de l'étage
-    public function setId($id) {
-        $this->id = $id;
-    }
-
-    // Fonction pour définir le nom de l'étage
-    public function setName($name) {
-        $this->name = $name;
-    }
-
-    // Fonction pour définir le niveau de l'étage
-    public function setLevel($level) {
+    // Setter pour le niveau de l'étage
+    public function setLevel(int $level): self {
         $this->level = $level;
+        return $this;
     }
 }
+
 ?>
