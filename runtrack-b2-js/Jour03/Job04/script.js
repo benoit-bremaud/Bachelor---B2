a722a7537e28cdecc29cc48fd3eea9f3014cc5dd
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-09-11 10:28:24 +0200

    feat: Import Board class in script.js
    In this commit, we've imported the Board class from "Board.js" in the script.js file. This allows us to use the Board class for managing the game board in our Tic Tac Toe game.

--- runtrack-b2-js/Jour03/Job04/script.js
+++ runtrack-b2-js/Jour03/Job04/script.js
@@ -3,4 +3,7 @@
 // Importez la classe Player depuis Player.js
 import Player from './Player.js';
 
+// Importez la classe Board depuis Board.js
+import Board from './Board.js';
+
 // Vous pouvez maintenant utiliser la classe Player dans ce fichier
