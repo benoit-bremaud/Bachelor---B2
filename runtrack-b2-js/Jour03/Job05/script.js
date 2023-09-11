03ba5803f90d4bb2bb2dc88df043616a79334783
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-09-11 10:38:42 +0200

    feat: Integrate Game class in script.js
    In this commit, we've integrated the Game class from "Game.js" in the script.js file. This allows us to create and manage a complete Tic Tac Toe game with players and a game board.

--- runtrack-b2-js/Jour03/Job05/script.js
+++ runtrack-b2-js/Jour03/Job05/script.js
@@ -1,9 +1,23 @@
 // script.js
 
 // Importez la classe Player depuis Player.js
-import Player from './class/Player';
+import Player from './Player.js';
 
 // Importez la classe Board depuis Board.js
-import Board from './class/Board';
+import Board from './Board.js';
 
-// Vous pouvez maintenant utiliser la classe Player dans ce fichier
+// Importez la classe Game depuis Game.js
+import Game from './Game.js';
+
+// Créez deux joueurs
+const playerX = new Player('Joueur X', 'X');
+const playerO = new Player('Joueur O', 'O');
+
+// Créez un plateau de jeu
+const gameBoard = new Board();
+
+// Créez une instance de jeu en utilisant les joueurs et le plateau
+const ticTacToeGame = new Game(playerX, playerO, gameBoard);
+
+// Exemple d'utilisation : initialisez une nouvelle partie
+ticTacToeGame.startNewGame();
