ebd35e5062c1de4ff96537fa7ab42b08f70b9278
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-09-11 10:18:35 +0200

    feat: Import Player class using ES6 modules
    In this commit, we've introduced ES6 module usage to import the Player class from "Player.js" into the "script.js" file. This allows us to use the Player class for managing players in the Tic Tac Toe game.

---
+++ runtrack-b2-js/Jour03/Job03/class/Player.js
@@ -0,0 +1,13 @@
+// Player.js
+
+class Player {
+    constructor(name, symbol) {
+      this.name = name;
+      this.symbol = symbol;
+      this.isCurrentPlayer = false; // Par défaut, ce n'est pas le tour de ce joueur
+    }
+  }
+  
+  // Exportez la classe Player pour pouvoir l'utiliser dans d'autres fichiers
+  export default Player;
+  
