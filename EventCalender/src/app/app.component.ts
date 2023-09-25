09140291dc7c1879d59ba8a41ba8281e85527213
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-09-25 16:14:43 +0200

    feat(i18n): implement French language and display format for calendar
    Updated the code to support the French language, including French display formats for days, hours, and button labels.

--- EventCalender/src/app/app.component.ts
+++ EventCalender/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CalendarEvent, CalendarView } from 'angular-calendar';
+import { isSameDay, isSameMonth } from 'date-fns';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'EventCalender';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  viewDate: Date = new Date();
+  view: CalendarView = CalendarView.Week;
+  CalendarView = CalendarView;
+
+  events: CalendarEvent[] = [];
+
+  activeDayIsOpen = false;
+
+  constructor() {
+    const event1 = {
+      title: "Cours de tennis",
+      start: new Date("2023-11-09T10:30"),
+      end: new Date("2023-11-09T17:30"),
+    }
+
+    this.events.push(event1);
+
+  }
+
+  setView(view: CalendarView) {
+    this.view = view;
+  }
+
+  dayClicked({ date, events }: {date:Date; events: CalendarEvent[]}): void {
+    if (isSameMonth(date, this.viewDate)) {
+      if ((isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) || events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+      }
+      this.viewDate = date;
+    }
+  }
+
 }
--- EventCalender/src/app/app.module.ts
+++ EventCalender/src/app/app.module.ts
@@ -3,6 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarDateFormatter, CalendarModule, CalendarNativeDateFormatter, DateAdapter, DateFormatterParams } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import localeFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeFr, 'fr');
+
+
+class CustomDateFormatter extends CalendarNativeDateFormatter {
+  public override dayViewHour({date, locale }: DateFormatterParams): string {
+    return new Intl.DateTimeFormat(locale, {hour: 'numeric', minute: 'numeric'}).format(date);
+  }
+
+  public override weekViewHour({date, locale}: DateFormatterParams): string {
+    return new Intl.DateTimeFormat(locale, {hour: 'numeric', minute: 'numeric'}).format(date);
+  }
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +28,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
   ],
-  providers: [],
+  providers: [
+    {provide: CalendarDateFormatter, useClass: CustomDateFormatter}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
