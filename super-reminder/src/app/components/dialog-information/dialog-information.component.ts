59bab188a7b614f217d5bbf760dd5a6d88b1d73b
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-09-28 12:43:05 +0200

    chore(code): added comments for clarity and documentation
    Added comments throughout the code to improve readability, provide context, and document important sections.

--- super-reminder/src/app/components/dialog-information/dialog-information.component.ts
+++ super-reminder/src/app/components/dialog-information/dialog-information.component.ts
@@ -1,27 +1,32 @@
+// Importing necessary modules and components from Angular and Angular Material
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+// Angular component declaration with selector, template, and styles
 @Component({
-  selector: 'app-dialog-information',
-  templateUrl: './dialog-information.component.html',
-  styleUrls: ['./dialog-information.component.css']
+  selector: 'app-dialog-information', // Selector for this component
+  templateUrl: './dialog-information.component.html', // Template file for this component
+  styleUrls: ['./dialog-information.component.css'] // Styles for this component
 })
 export class DialogInformationComponent implements OnInit {
 
-  title: string;
-  text: string;
-  labelOk: string;
+  title: string; // Variable to hold the title for the dialog
+  text: string; // Variable to hold the text content for the dialog
+  labelOk: string; // Variable to hold the label for the 'OK' button
 
+  // Constructor for the component, injecting MAT_DIALOG_DATA to receive data
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any // Injecting data for the dialog
   ) {
-    this.title = data.title;
-    this.text = data.text;
-    this.labelOk = data.labelOK;
+    // Assigning values from the injected data to component variables
+    this.title = data.title; // Assigning title from the injected data
+    this.text = data.text; // Assigning text content from the injected data
+    this.labelOk = data.labelOK; // Assigning label for the 'OK' button from the injected data
   }
 
+  // Lifecycle hook: Angular calls this when initializing the component
   ngOnInit(): void {
-      
+    // Initialization logic can be placed here if needed
   }
 
 }
